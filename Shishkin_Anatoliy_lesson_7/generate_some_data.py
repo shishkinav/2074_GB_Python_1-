ca0a25a9693428c0c35202fa00a34f4b6dda8f43
Anatoliy Shishkin <shishkin_anatoli@mail.ru>
2022-02-07 23:07:50 +0300

    fix source files

--- Shishkin_Anatoliy_lesson_7/generate_some_data.py
+++ Shishkin_Anatoliy_lesson_7/generate_some_data.py
@@ -1,5 +1,6 @@
+import random
 from string import ascii_lowercase, digits
-from start_7_1 import BASE_DIR, os, random
+from code_7 import BASE_DIR, os
 
 
 letters = ''.join([ascii_lowercase, digits])
